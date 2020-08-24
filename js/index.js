d8061de0b9f224fd5387066bdada0b920dd5c339
ranji00 <ranjith@codingmart.com>
2020-08-24 18:45:14 +0530

    Added footer and css

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 $(function(){
 
-    $("#announcement").click(function(){
-        console.log("Clicked");
-    })
+  
     
     $("#side-toc ul li a").click(function(){
         $(this).parent().addClass("is-selected");
