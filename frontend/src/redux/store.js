a2778b7dcd239c197b1305b99b1ffe1223ff4357
RanjithGovindaraj <kmgrranjith@gmail.com>
2020-10-13 14:00:01 +0530

    Removed redux devtools

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,17 @@
 import { createStore,applyMiddleware } from "redux";
-import { logger } from "redux-logger";
+// import { logger } from "redux-logger";
 import  rootReducer from "./rootReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+
+// const store = createStore(
+// 	rootReducer,
+// 	composeWithDevTools(applyMiddleware(logger,thunk))
+// );
 
 const store = createStore(
 	rootReducer,
-	composeWithDevTools(applyMiddleware(logger,thunk))
+	applyMiddleware(thunk)
 );
 
 export default store
